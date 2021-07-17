9db7b834d0caf287cc04fee7132d5afdf11d986d
christopher <christopheralejandro.lariosperez@mx.bosch.com>
2021-07-17 14:08:10 -0500

    adding cors,mongoose,body-parser dependencies

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const bodyParser = require('body-parser')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
