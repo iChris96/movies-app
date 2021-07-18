f587de44b91349b790a020d216af83238ff176ce
christopher <christopheralejandro.lariosperez@mx.bosch.com>
2021-07-18 16:31:30 -0500

    handling eslint and prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import bodyParser from 'body-parser'
-import express from 'express';
-import { Request, Response } from 'express';
-import cors from 'cors'
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 3000
-
-
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+    res.send('hello world');
+    res.send('hello world');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
