362b01c004e1987dcecef6a7b45fdf4654807729
christopher <christopheralejandro.lariosperez@mx.bosch.com>
2021-09-28 21:02:52 -0500

    set token expiration to 60 seconds

--- src/consts/auth.ts
+++ src/consts/auth.ts
@@ -3,8 +3,8 @@ dotenv.config({ path: '.env' });
 
 export const SALT_ROUNDS = 10;
 
-export const HEADER_TOKEN = 'auth-token';
+export const HEADER_TOKEN = 'Authorization';
 
-export const TOKEN_EXP = 60 * 60 * 24; // 1 day
+export const TOKEN_EXP = 60; // 60 * 60 * 24; // 1 day
 
 export const TOKEN_SECRET: string = process.env.SECRET_TOKEN as string;
